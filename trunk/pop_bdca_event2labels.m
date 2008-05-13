fca6a2b2378ab1ae7977cddf1827063aedbd8c93
mistermads <mistermads@2de362fa-e849-0410-9a33-e30d18c51d2b>
2008-05-13 17:00:36 +0000

    AM   pop_bdca_event2labels.m

---
+++ trunk/pop_bdca_event2labels.m
@@ -0,0 +1,48 @@
+function [EEG,LASTCOM] = pop_bdca_event2labels(EEG,contrast,versus)
+% [EEG,LASTCOM] = pop_bdca_event2labels(EEG,contrast,versus)
+
+% process commandline
+if nargin < 3
+  % 'Contrast event types','Versus event types',
+  prompt={'Contrast events','Versus'};
+  name='Input for pop_bdca_event2labels';
+  numlines=1;
+  defaultanswer={'',''};
+  try 
+    defaultanswer{1} = sprintf('%i',contrast);
+    defaultanswer{2} = sprintf('%i',versus);
+  end
+  answer=inputdlg(prompt,name,numlines,defaultanswer);
+  if isempty(answer), return, end
+  contrast  = eval(sprintf('[%s]',answer{1}));
+  versus     = eval(sprintf('[%s]',answer{2}));
+end
+
+
+% find labels etc.
+labels = nan * zeros(1,EEG.trials);
+for epo = 1:EEG.trials
+  if length(EEG.epoch(epo).eventlatency)>1
+    idx = find([EEG.epoch(epo).eventlatency{:}] == 0);
+    tmp = [EEG.epoch(epo).eventtype{idx}];
+  else
+    idx = find([EEG.epoch(epo).eventlatency] == 0); 
+    tmp = [EEG.epoch(epo).eventtype(idx)];
+  end
+  
+  if ismember(tmp,contrast)
+    labels(epo) = 1;
+  end
+  if ismember(tmp,versus)
+    labels(epo) = 0;
+  end
+end
+
+
+EEG.bdca.labels = labels;
+
+try
+  LASTCOM = sprintf('EEG = pop_bdca_event2labels(EEG,[%s],[%s]);',answer{1},answer{2});
+catch
+  LASTCOM = '';
+end
